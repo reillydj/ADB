2b5cea6f6c5ae7fdde63066886ea162f74924ced
David Reilly <reillydj116@gmail.com>
2018-09-15 13:17:12 -0700

    check if contains, else throw

--- src/main/java/com/dreilly/adb/AdbService.java
+++ src/main/java/com/dreilly/adb/AdbService.java
@@ -1,15 +1,38 @@
 package com.dreilly.adb;
 
+import com.dreilly.adb.thrift.AdbException;
 import org.apache.thrift.TException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AdbService implements com.dreilly.adb.thrift.AdbService.Iface {
 
-    public int get(String id) throws TException {
-        return 2;
+    private static Map<String, Integer> backing;
+
+    public AdbService() {
+        backing = new HashMap<String, Integer>();
     }
 
-    public void put(String id, int value) throws TException {
+    public int get(String id) throws AdbException {
+        if (backing.containsKey(id)) {
+            return backing.get(id);
+        } else {
+            throw new AdbException(String.format("Record not found: %s", id));
+        }
+    }
+
+    public void put(String id, int value) {
         
+    }
+
+    public static void main(String[] args) {
+        AdbService service = new AdbService();
+        try {
+            System.out.println(service.get("1"));
+        } catch (TException e) {
+            System.out.println("caught");
+        }
     }
 
 }
