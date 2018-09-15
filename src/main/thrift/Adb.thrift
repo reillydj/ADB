b2841a7f0be9934d9bd89c679dc44423e03faea2
David Reilly <reillydj116@gmail.com>
2018-09-15 13:16:56 -0700

    add exception class

--- src/main/thrift/Adb.thrift
+++ src/main/thrift/Adb.thrift
@@ -1,8 +1,12 @@
 namespace java com.dreilly.adb.thrift
 
 typedef i32 int
-service AdbService
-{
+
+exception AdbException {
+    1: string description
+}
+
+service AdbService {
     int get(1:string id),
-    void put(1:string id, 2:int value),
+    void put(1:string id, 2:int value) throws (1: AdbException ex),
 }
