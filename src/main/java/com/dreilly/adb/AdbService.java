c89aff023d01f109e3e4bc43558c256e71d2b394
David Reilly <reillydj116@gmail.com>
2018-09-15 13:01:06 -0700

    remove bad import

--- src/main/java/com/dreilly/adb/AdbService.java
+++ src/main/java/com/dreilly/adb/AdbService.java
@@ -1,6 +1,5 @@
 package com.dreilly.adb;
 
-import com.dreilly.adb.thrift.AdbService;
 import org.apache.thrift.TException;
 
 public class AdbService implements com.dreilly.adb.thrift.AdbService.Iface {
